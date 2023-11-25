20a42195fdb1ecefcd1b1745fe8a21c5269a14ec
Susana Viruglio <susanaviruglio@hotmail.com>
2023-11-25 12:49:45 +0000

    import app variable which I created within my librarymanager

---
+++ run.py
@@ -0,0 +1,12 @@
+#I have to import os , in order to utilize environment variables within this file and
+# import app variable which I created within librarymanager
+import os
+from librarymanager import app 
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=os.environ.get("PORT"),
+        debug=os.environ.get("DEBUG")
+    )
