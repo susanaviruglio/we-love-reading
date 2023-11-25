cb2b90135e20b51cc086fecf154cea646eed3d75
Susana Viruglio <susanaviruglio@hotmail.com>
2023-11-25 12:53:17 +0000

    added my manager routes for my three tables

---
+++ librarymanager/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from librarymanager import app, db
+from librarymanager.models import Book, Users, Review
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    # if I change the name it will become the homepage.
