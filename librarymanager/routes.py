aec71f2cfe590e0e719c8e1b33e1077d3e29d784
Susana Viruglio <susanaviruglio@hotmail.com>
2023-11-25 18:53:19 +0000

    added profile link and add_reviews link

--- librarymanager/routes.py
+++ librarymanager/routes.py
@@ -6,3 +6,13 @@ from librarymanager.models import Book, Users, Review
 def home():
     return render_template("base.html")
     # if I change the name it will become the homepage.
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/add_review", methods=["GET", "POST"])
+def add_review():
+    return render_template("add_review.html")
