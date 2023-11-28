5c590bc39e12614a57ec9df29a5feaa0a511f9c4
Susana Viruglio <susanaviruglio@hotmail.com>
2023-11-28 22:45:17 +0000

    ready to commit

--- librarymanager/__init__.py
+++ librarymanager/__init__.py
@@ -9,8 +9,13 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-
+if os.environ.get("DEVELOPMENT") == "True":
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+ else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 # I have created an instance of the imported SQLAlchemy() class, which would be
 # assigned to a variable of 'db'.
