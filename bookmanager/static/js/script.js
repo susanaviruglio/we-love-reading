052d10ab441b0e09a57a22301377d78cf59ce0f2
Susana Viruglio <susanaviruglio@hotmail.com>
2023-12-03 14:21:24 +0000

    added function for datalist in add_review from materialize

--- bookmanager/static/js/script.js
+++ bookmanager/static/js/script.js
@@ -2,4 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav= document.querySelector('.sidenav');
     M.Sidenav.init(sidenav);
+
+
+  // select initialization
+  let datalists = document.querySelectorAll("datalist");
+  M.FormSelect.init(datalists);
   });
+
+
