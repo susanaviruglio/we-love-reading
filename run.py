5112cc82b58267778792671e13a36fbc6419dd10
Susana Viruglio <susanaviruglio@hotmail.com>
2023-12-07 21:13:23 +0000

    python validation

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 #I have to import os , in order to utilize environment variables within this file and
-# import app variable which I created within librarymanager
+# import app variable which I created within librarymanager.
 import os
 from bookmanager import app 
 
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"),
-        port=os.environ.get("PORT"),
-        debug=os.environ.get("DEBUG")
-    )
+        host = os.environ.get("IP"),
+        port = os.environ.get("PORT"),
+        debug = os.environ.get("DEBUG")
+)
