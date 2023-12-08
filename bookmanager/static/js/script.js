815baae273a51b0adf17f297432abcbdfa4a9d74
Susana Viruglio <susanaviruglio@hotmail.com>
2023-12-08 23:49:58 +0000

    deleted function for datalist in add review site becuase it was duplicating the list

--- bookmanager/static/js/script.js
+++ bookmanager/static/js/script.js
@@ -2,11 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav= document.querySelector('.sidenav');
     M.Sidenav.init(sidenav);
-
-
-  // select initialization
-  let datalists = document.querySelectorAll("datalist");
-  M.FormSelect.init(datalists);
+ 
   });
 
 
